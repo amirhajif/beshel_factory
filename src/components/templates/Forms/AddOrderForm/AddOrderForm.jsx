2e12a0733d7500b5b3bad5cda83f566c941ab2a0
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-12-13 09:54:19 +0330

    Add Order Api

--- src/components/templates/Forms/AddOrderForm/AddOrderForm.jsx
+++ src/components/templates/Forms/AddOrderForm/AddOrderForm.jsx
@@ -2,7 +2,7 @@
 import Select from '@/components/shared/Select'
 import React, { useEffect, useState } from 'react'
 
-import DatePicker, { DateObject } from "react-multi-date-picker"
+import DatePicker from "react-multi-date-picker"
 import persian from "react-date-object/calendars/persian"
 import persian_fa from "react-date-object/locales/persian_fa"
 import Gregorian from 'react-date-object/calendars/gregorian'
@@ -13,40 +13,59 @@ import Button from "@/components/shared/Button";
 import Label from "@/components/shared/Label";
 import TextField from "@/components/shared/TextField";
 import AddOrderFormFields from '@/models/AddOrderFormFields'
-import { getAllParts, getAllClients } from '@/apis/sampleApi'
+import { getAllParts, getAllClients, addOrder } from '@/apis/sampleApi'
+import sendNotif from '@/utils/sendNotif'
 
 const AddOrderForm = ({
     formClassName
 }) => {
-    let [start, setStart] = useState(new DateObject())
-    let [finish, setFinish] = useState(new DateObject())
+    let [started_at, setStarted_at] = useState('')
+    let [ended_at, setEnded_at] = useState('')
     const [parts, setParts] = useState([])
     const [companies, setCompanies] = useState([])
+
+    const resetDate = () => {
+        setStarted_at('')
+        setEnded_at('')
+    }
     const handleSubmit = async (e) => {
         e.preventDefault()
         let formsElements = e.target.elements;
 
-        let productName = formsElements.namedItem(
+        let part = formsElements.namedItem(
             AddOrderFormFields?.productName?.title
         )?.value;
-        let companyName = formsElements.namedItem(
+        let client = formsElements.namedItem(
             AddOrderFormFields?.companyName?.title
         )?.value;
         let count = formsElements.namedItem(
             AddOrderFormFields?.count?.title
         )?.value
 
-        console.log(productName, companyName, count)
-        console.log(start.convert(Gregorian, Gregorian_en).format("YYYY-MM-DD"))
-        console.log(finish.convert(Gregorian, Gregorian_en).format("YYYY-MM-DD"))
+        const response = await addOrder({
+            'count': count,
+            'started_at': started_at.convert(Gregorian, Gregorian_en).format("YYYY-MM-DD"),
+            'ended_at': ended_at.convert(Gregorian, Gregorian_en).format("YYYY-MM-DD"),
+            "parts": part,
+            "client": client
+        })
 
+        response != undefined ?
+            sendNotif('سفارش ثبت شد', 'success', true, 'center', 2000)
+            :
+            sendNotif('در ثبت سفارش شما مشکلی پیش آمده', 'error', true, 'center', 2000)
+
+        resetDate()
     }
+
     useEffect(() => {
+        // getParts
         const cachedParts = getAllParts()
         cachedParts.then((res) => {
             setParts(res.data.data)
         })
 
+        // get Companies(clients)
         const cachedCompanies = getAllClients()
         cachedCompanies.then((res) => {
             setCompanies(res.data.data)
@@ -55,8 +74,8 @@ const AddOrderForm = ({
     }, [])
 
     return (
-
         <form form className={formClassName} onSubmit={handleSubmit} >
+            {/* part */}
             <Select
                 parentClassName="w-full md:w-1/2 px-3 mb-6 md:mb-0"
                 labelClassName="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -66,7 +85,7 @@ const AddOrderForm = ({
                 selectId="partName"
                 options={parts}
             />
-
+            {/* client */}
             <Select
                 parentClassName="w-full md:w-1/2 px-3 mb-6 md:mb-0"
                 labelClassName="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -82,25 +101,25 @@ const AddOrderForm = ({
                 <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" forValue="startDate" text="تاریخ شروع" />
                 <DatePicker
                     style={{ cursor: 'pointer', padding: '20px' }}
-                    value={start}
-                    onChange={setStart}
+                    value={started_at}
+                    onChange={setStarted_at}
                     calendar={persian}
                     locale={persian_fa}
                     className="red"
 
                 />
             </div>
-            {/* finish date */}
+            {/* end date */}
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 ">
                 <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" forValue="finishDate" text="تاریخ پایان" />
                 <DatePicker
                     style={{ cursor: 'pointer', padding: '20px' }}
-                    value={finish}
-                    onChange={setFinish}
+                    value={ended_at}
+                    onChange={setEnded_at}
                     calendar={persian}
                     locale={persian_fa}
                     className="red"
-                    minDate={finish}
+                    minDate={started_at}
                 />
             </div>
 
@@ -110,7 +129,7 @@ const AddOrderForm = ({
                 <TextField name="count" id="count" placeholder="۱۵" className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" />
             </div>
             <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
-                <Button className="bg-transparent hover:bg-green-500 w-full text-green-700 font-semibold hover:text-white mt-6  py-3 px-4 border border-green-500 hover:border-transparent rounded">ثبت سفارش</Button>
+                <Button className="bg-transparent hover:bg-green-500 w-full text-green-700 font-semibold hover:text-white mt-6  py-3 px-4 border border-green-500 hover:border-transparent rounded" type='reset'>ثبت سفارش</Button>
             </div>
         </form >
     )
