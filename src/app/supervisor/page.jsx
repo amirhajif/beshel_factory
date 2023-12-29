9461cb28943b06a550209d7af7596b5cee7a352d
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-12-29 12:13:54 +0330

    add Filter to Supervisor

--- src/app/supervisor/page.jsx
+++ src/app/supervisor/page.jsx
@@ -6,6 +6,8 @@ import SupervisorNavbarItems from "@/constants/SupervisorNavbarItems";
 import Filter from "@/components/shared/Filter";
 import Routes from "@/constants/Routes";
 import getMachines from "@/apis/getMachines";
+import getOrders from "@/apis/getOrders";
+import { getOperators } from "@/apis/getOperators";
 
 const Supervisor = async () => {
   const DashboardWrapper = dynamic(
@@ -18,11 +20,17 @@ const Supervisor = async () => {
   const machineRequest = await getMachines();
   const machines = machineRequest?.data?.results;
 
+  const ordersRequest = await getOrders();
+  const orders = ordersRequest?.data?.results;
+
+  const operatorsRequest = await getOperators()
+  const operators = operatorsRequest?.data?.results;
+
   return (
     <DashboardWrapper navItems={SupervisorNavbarItems}>
       <DashboardContent>
         <div className="costume-scroll overflow-scroll xs:w-[100%] w-[95%] h-[90%]">
-          <Filter options={{ machines: machines }} />
+          <Filter options={{ machines: machines, orders: orders, operators: operators }} />
           <ReportMiniTable baseRoute={Routes?.Supervisor} />
         </div>
       </DashboardContent>
