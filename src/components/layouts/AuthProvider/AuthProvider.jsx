d6544371a15e7341fa1179e4ee48e8876c566278
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-01-01 11:41:40 +0330

    Handle Token not Set or Logout redirect to login

--- src/components/layouts/AuthProvider/AuthProvider.jsx
+++ src/components/layouts/AuthProvider/AuthProvider.jsx
@@ -2,11 +2,13 @@
 
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 const AuthProvider = ({ children }) => {
   const router = useRouter();
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      // logic for redirect , if credential are not set => router.push("/");
+    const token = Cookies.get("token");
+    if (!token) {
+      router.push("/");
     }
   }, []);
   return <section>{children}</section>;
