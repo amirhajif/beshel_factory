591750be9fd4d47b9775444974922e80a0f5c829
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-04-16 17:50:29 +0330

    Fix Operator Apis

--- src/apis/getOperators.js
+++ src/apis/getOperators.js
@@ -4,7 +4,7 @@ const {
 
 export const getOperators = async () => {
     try {
-        return await axiosInterceptorInstance.get("/operators/filterData?all_data")
+        return await axiosInterceptorInstance.get("/operators-and-supervisors?all_data")
 
     } catch (error) {
         throw new Error(error);
